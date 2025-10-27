dffd361bea58c2f9b9772e92038fa038ca82e478
Seasonal <seasoank@gmail.com>
2025-10-27 06:01:58 -0700

    something

--- src/overlay/overlay.hpp
+++ src/overlay/overlay.hpp
@@ -4,47 +4,156 @@
 
 #pragma comment(lib, "d3d11.lib")
 
-ImGuiKey vk_to_imguikey(DWORD vk) {
-    if (vk >= 'A' && vk <= 'Z') return static_cast<ImGuiKey>(ImGuiKey_A + (vk - 'A'));
+// WTF WHY IS THIS UNDOCUMENTED (https://github.com/ocornut/imgui/blob/master/backends/imgui_impl_win32.cpp#L436)
+// if i dont add _Public to name it fails with: multiple definition of `ImGui_ImplWin32_KeyEventToImGuiKey(unsigned long long, long long)';
+// wow ig ocornut just doesnt like documenting
+ImGuiKey ImGui_ImplWin32_KeyEventToImGuiKey_Public(WPARAM wParam, LPARAM lParam)
+{
+    if ((wParam == VK_RETURN) && (HIWORD(lParam) & KF_EXTENDED))
+        return ImGuiKey_KeypadEnter;
 
-    if (vk >= '0' && vk <= '9') return static_cast<ImGuiKey>(ImGuiKey_0 + (vk - '0'));
+    const int scancode = (int)LOBYTE(HIWORD(lParam));
 
-    switch (vk) {
-        case VK_LEFT:  return ImGuiKey_LeftArrow;
+    switch (wParam)
+    {
+        case VK_TAB: return ImGuiKey_Tab;
+        case VK_LEFT: return ImGuiKey_LeftArrow;
         case VK_RIGHT: return ImGuiKey_RightArrow;
-        case VK_UP:    return ImGuiKey_UpArrow;
-        case VK_DOWN:  return ImGuiKey_DownArrow;
-
-        case VK_BACK:   return ImGuiKey_Backspace;
-        case VK_TAB:    return ImGuiKey_Tab;
+        case VK_UP: return ImGuiKey_UpArrow;
+        case VK_DOWN: return ImGuiKey_DownArrow;
+        case VK_PRIOR: return ImGuiKey_PageUp;
+        case VK_NEXT: return ImGuiKey_PageDown;
+        case VK_HOME: return ImGuiKey_Home;
+        case VK_END: return ImGuiKey_End;
+        case VK_INSERT: return ImGuiKey_Insert;
+        case VK_DELETE: return ImGuiKey_Delete;
+        case VK_BACK: return ImGuiKey_Backspace;
+        case VK_SPACE: return ImGuiKey_Space;
         case VK_RETURN: return ImGuiKey_Enter;
         case VK_ESCAPE: return ImGuiKey_Escape;
-        case VK_SPACE:  return ImGuiKey_Space;
-        case VK_DELETE: return ImGuiKey_Delete;
-        case VK_HOME:   return ImGuiKey_Home;
-        case VK_END:    return ImGuiKey_End;
-        case VK_PRIOR:  return ImGuiKey_PageUp;
-        case VK_NEXT:   return ImGuiKey_PageDown;
-        case VK_INSERT: return ImGuiKey_Insert;
-
-        case VK_SHIFT:   return ImGuiKey_LeftShift;
-        case VK_CONTROL: return ImGuiKey_LeftCtrl;
-        case VK_MENU:    return ImGuiKey_LeftAlt;
-
-        case VK_OEM_1:  return ImGuiKey_Semicolon;
-        case VK_OEM_PLUS: return ImGuiKey_Equal;
+        //case VK_OEM_7: return ImGuiKey_Apostrophe;
         case VK_OEM_COMMA: return ImGuiKey_Comma;
-        case VK_OEM_MINUS: return ImGuiKey_Minus;
+        //case VK_OEM_MINUS: return ImGuiKey_Minus;
         case VK_OEM_PERIOD: return ImGuiKey_Period;
-        case VK_OEM_2: return ImGuiKey_Slash;
-        case VK_OEM_3: return ImGuiKey_GraveAccent;
-        case VK_OEM_4: return ImGuiKey_LeftBracket;
-        case VK_OEM_5: return ImGuiKey_Backslash;
-        case VK_OEM_6: return ImGuiKey_RightBracket;
-        case VK_OEM_7: return ImGuiKey_Apostrophe;
-
-        default: return ImGuiKey_None;
+        //case VK_OEM_2: return ImGuiKey_Slash;
+        //case VK_OEM_1: return ImGuiKey_Semicolon;
+        //case VK_OEM_PLUS: return ImGuiKey_Equal;
+        //case VK_OEM_4: return ImGuiKey_LeftBracket;
+        //case VK_OEM_5: return ImGuiKey_Backslash;
+        //case VK_OEM_6: return ImGuiKey_RightBracket;
+        //case VK_OEM_3: return ImGuiKey_GraveAccent;
+        case VK_CAPITAL: return ImGuiKey_CapsLock;
+        case VK_SCROLL: return ImGuiKey_ScrollLock;
+        case VK_NUMLOCK: return ImGuiKey_NumLock;
+        case VK_SNAPSHOT: return ImGuiKey_PrintScreen;
+        case VK_PAUSE: return ImGuiKey_Pause;
+        case VK_NUMPAD0: return ImGuiKey_Keypad0;
+        case VK_NUMPAD1: return ImGuiKey_Keypad1;
+        case VK_NUMPAD2: return ImGuiKey_Keypad2;
+        case VK_NUMPAD3: return ImGuiKey_Keypad3;
+        case VK_NUMPAD4: return ImGuiKey_Keypad4;
+        case VK_NUMPAD5: return ImGuiKey_Keypad5;
+        case VK_NUMPAD6: return ImGuiKey_Keypad6;
+        case VK_NUMPAD7: return ImGuiKey_Keypad7;
+        case VK_NUMPAD8: return ImGuiKey_Keypad8;
+        case VK_NUMPAD9: return ImGuiKey_Keypad9;
+        case VK_DECIMAL: return ImGuiKey_KeypadDecimal;
+        case VK_DIVIDE: return ImGuiKey_KeypadDivide;
+        case VK_MULTIPLY: return ImGuiKey_KeypadMultiply;
+        case VK_SUBTRACT: return ImGuiKey_KeypadSubtract;
+        case VK_ADD: return ImGuiKey_KeypadAdd;
+        case VK_LSHIFT: return ImGuiKey_LeftShift;
+        case VK_LCONTROL: return ImGuiKey_LeftCtrl;
+        case VK_LMENU: return ImGuiKey_LeftAlt;
+        case VK_LWIN: return ImGuiKey_LeftSuper;
+        case VK_RSHIFT: return ImGuiKey_RightShift;
+        case VK_RCONTROL: return ImGuiKey_RightCtrl;
+        case VK_RMENU: return ImGuiKey_RightAlt;
+        case VK_RWIN: return ImGuiKey_RightSuper;
+        case VK_APPS: return ImGuiKey_Menu;
+        case '0': return ImGuiKey_0;
+        case '1': return ImGuiKey_1;
+        case '2': return ImGuiKey_2;
+        case '3': return ImGuiKey_3;
+        case '4': return ImGuiKey_4;
+        case '5': return ImGuiKey_5;
+        case '6': return ImGuiKey_6;
+        case '7': return ImGuiKey_7;
+        case '8': return ImGuiKey_8;
+        case '9': return ImGuiKey_9;
+        case 'A': return ImGuiKey_A;
+        case 'B': return ImGuiKey_B;
+        case 'C': return ImGuiKey_C;
+        case 'D': return ImGuiKey_D;
+        case 'E': return ImGuiKey_E;
+        case 'F': return ImGuiKey_F;
+        case 'G': return ImGuiKey_G;
+        case 'H': return ImGuiKey_H;
+        case 'I': return ImGuiKey_I;
+        case 'J': return ImGuiKey_J;
+        case 'K': return ImGuiKey_K;
+        case 'L': return ImGuiKey_L;
+        case 'M': return ImGuiKey_M;
+        case 'N': return ImGuiKey_N;
+        case 'O': return ImGuiKey_O;
+        case 'P': return ImGuiKey_P;
+        case 'Q': return ImGuiKey_Q;
+        case 'R': return ImGuiKey_R;
+        case 'S': return ImGuiKey_S;
+        case 'T': return ImGuiKey_T;
+        case 'U': return ImGuiKey_U;
+        case 'V': return ImGuiKey_V;
+        case 'W': return ImGuiKey_W;
+        case 'X': return ImGuiKey_X;
+        case 'Y': return ImGuiKey_Y;
+        case 'Z': return ImGuiKey_Z;
+        case VK_F1: return ImGuiKey_F1;
+        case VK_F2: return ImGuiKey_F2;
+        case VK_F3: return ImGuiKey_F3;
+        case VK_F4: return ImGuiKey_F4;
+        case VK_F5: return ImGuiKey_F5;
+        case VK_F6: return ImGuiKey_F6;
+        case VK_F7: return ImGuiKey_F7;
+        case VK_F8: return ImGuiKey_F8;
+        case VK_F9: return ImGuiKey_F9;
+        case VK_F10: return ImGuiKey_F10;
+        case VK_F11: return ImGuiKey_F11;
+        case VK_F12: return ImGuiKey_F12;
+        case VK_F13: return ImGuiKey_F13;
+        case VK_F14: return ImGuiKey_F14;
+        case VK_F15: return ImGuiKey_F15;
+        case VK_F16: return ImGuiKey_F16;
+        case VK_F17: return ImGuiKey_F17;
+        case VK_F18: return ImGuiKey_F18;
+        case VK_F19: return ImGuiKey_F19;
+        case VK_F20: return ImGuiKey_F20;
+        case VK_F21: return ImGuiKey_F21;
+        case VK_F22: return ImGuiKey_F22;
+        case VK_F23: return ImGuiKey_F23;
+        case VK_F24: return ImGuiKey_F24;
+        case VK_BROWSER_BACK: return ImGuiKey_AppBack;
+        case VK_BROWSER_FORWARD: return ImGuiKey_AppForward;
+        default: break;
     }
+
+    switch (scancode)
+    {
+    case 41: return ImGuiKey_GraveAccent;  // VK_OEM_8 in EN-UK, VK_OEM_3 in EN-US, VK_OEM_7 in FR, VK_OEM_5 in DE, etc.
+    case 12: return ImGuiKey_Minus;
+    case 13: return ImGuiKey_Equal;
+    case 26: return ImGuiKey_LeftBracket;
+    case 27: return ImGuiKey_RightBracket;
+    case 86: return ImGuiKey_Oem102;
+    case 43: return ImGuiKey_Backslash;
+    case 39: return ImGuiKey_Semicolon;
+    case 40: return ImGuiKey_Apostrophe;
+    case 51: return ImGuiKey_Comma;
+    case 52: return ImGuiKey_Period;
+    case 53: return ImGuiKey_Slash;
+    default: break;
+    }
+
+    return ImGuiKey_None;
 }
 
 namespace Overlay {
@@ -73,7 +182,7 @@ namespace Overlay {
         ImGuiIO& io = ImGui::GetIO();
         bool down = (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN);
 
-        ImGuiKey key = vk_to_imguikey(kb->vkCode);
+        ImGuiKey key = ImGui_ImplWin32_KeyEventToImGuiKey_Public(kb->vkCode, (LPARAM)kb->flags << 16 | kb->scanCode);
         if (key != ImGuiKey_None)
             io.AddKeyEvent(key, down);
 
@@ -113,18 +222,20 @@ namespace Overlay {
     }
 
     inline LRESULT CALLBACK MouseLL(int code, WPARAM wParam, LPARAM lParam) {
-        if (code == HC_ACTION) {
-            auto* ms = reinterpret_cast<MSLLHOOKSTRUCT*>(lParam);
-            mousePos = ImVec2(static_cast<float>(ms->pt.x), static_cast<float>(ms->pt.y));
-            switch (wParam) {
-                case WM_LBUTTONDOWN: leftDown = true; break;
-                case WM_LBUTTONUP:   leftDown = false; break;
-                case WM_RBUTTONDOWN: rightDown = true; break;
-                case WM_RBUTTONUP:   rightDown = false; break;
-            }
+        if (code != HC_ACTION)
+            return CallNextHookEx(nullptr, code, wParam, lParam);
+
+        auto* ms = reinterpret_cast<MSLLHOOKSTRUCT*>(lParam);
+        mousePos = ImVec2(static_cast<float>(ms->pt.x), static_cast<float>(ms->pt.y));
+        switch (wParam) {
+            case WM_LBUTTONDOWN: leftDown = true; break;
+            case WM_LBUTTONUP:   leftDown = false; break;
+            case WM_RBUTTONDOWN: rightDown = true; break;
+            case WM_RBUTTONUP:   rightDown = false; break;
         }
 
-
+        if (Menu::open)
+            return 1;
         return CallNextHookEx(nullptr, code, wParam, lParam);
     }
 
